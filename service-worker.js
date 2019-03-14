be1dbf14efc3107417ba81319d002e1e11c7b13c
jssan <jssantosqueiroz@gmail.com>
2019-03-13 23:29:59 -0300

    Correção do formulário de contato

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,8 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', function (evt) {
   console.log('The service worker is serving the asset.');
-  evt.respondWith(fromCache(evt.request) || fetch(evt.request));
+  console.log(evt);
+  evt.respondWith(fromCache(evt) || fetch(evt.request));
   evt.waitUntil(
     update(evt.request)
       .then(console.log('served'))
@@ -23,9 +24,28 @@ self.addEventListener('fetch', function (evt) {
 
 
 function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request);
-  });
+  caches.match(event.request).then(function (res) {
+    return res || requestBackend(event);
+  })
+}
+
+
+function requestBackend(event) {
+  let url = event.request.clone();
+  return fetch(url).then(function (res) {
+    //if not a valid response send the error
+    if (!res || res.status !== 200 || res.type !== 'basic') {
+      return res;
+    }
+
+    let response = res.clone();
+
+    caches.open(CACHE_VERSION).then(function (cache) {
+      cache.put(event.request, response);
+    });
+
+    return res;
+  })
 }
 
 function update(request) {
